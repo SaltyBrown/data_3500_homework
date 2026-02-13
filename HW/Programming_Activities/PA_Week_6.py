8c26153b23fea5b37e4fcac510c8c78f0e04f67c
Sky Brown <skywizzzy@gmail.com>
2026-02-13 23:36:08 +0000

    PA 6 done

---
+++ HW/Programming_Activities/PA_Week_6.py
@@ -0,0 +1,22 @@
+# -Activity 1
+def welcome_fctn(name):
+    print("Welcome", name)
+    
+welcome_fctn("Sky")
+
+
+# -Activity 2
+def welcome_fctn(name):
+    welcome_message = "Welcome " + name
+    return welcome_message
+    
+print(welcome_fctn("Sky"))
+
+
+# -Activity 3
+def welcome_fctn(name, age, favorite_color):
+    welcome_message = "Welcome " + name + " you are " + str(age) + " years old, and " + favorite_color + " is your favorite color"
+    return welcome_message
+    
+print(welcome_fctn("Sky", 24, "Pastel Yellow"))
+
